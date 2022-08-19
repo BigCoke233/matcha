f611125f653a9350870742c1e37480d9cd681bff
Eltrac Koalar <hi@guhub.cn>
2022-08-19 16:14:42 +0800

    :dizzy: Add: 完成 journal 样式

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,13 +1,14 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<div id="sidebar" role="complementary">
-    <header id="header" class="clearfix"><?php Matcha::siteName(); ?></header>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; 
+if(!$this->options->customStyle=='journal'): ?>
+<div id="sidebar" class="sidebar" role="complementary">
+    <header id="header" class="header clearfix"><?php Matcha::siteName(); ?></header>
     <!-- 侧边栏页面导航 -->
     <footer class="sidebar-footer">
       <section class="widget widget-nav">
         <ul class="widget-list"><?php Matcha::pageNav($this, "li"); ?></ul>
       </section>
       <!-- 版权信息 -->
-	  <section class="widget sidebar-foot">
+	    <section class="widget sidebar-foot">
         <ul class="widget-list">
             <li>Theme <a rel="nofollow" target="_blank" href="https://github.com/BigCoke233/matcha" class="no-linkTarget">Matcha</a> by <a  target="_blank" href="https://https://github.com/BigCoke233/" class="no-linkTarget">Eltrac</a></li>
             <li>Powered by <a rel="nofollow" target="_blank" href="http://www.typecho.org" class="no-linkTarget">Typecho</a></li>
@@ -15,6 +16,21 @@
       </section>
     </footer>
 </div>
+<?php else: ?>
+<div id="sidebar" class="sidebar-journal" role="complementary">
+  <section class="widget widget-nav sidebar-nav">
+    <header id="header" class="header-journal"><?php Matcha::siteName(); ?></header>
+    <ul class="widget-list"><?php Matcha::pageNav($this, "li"); ?></ul>
+  </section>
+  <!-- 版权信息 -->
+	<section class="widget sidebar-foot">
+    <ul class="widget-list">
+      <li>Theme <a rel="nofollow" target="_blank" href="https://github.com/BigCoke233/matcha" class="no-linkTarget">Matcha</a> by <a  target="_blank" href="https://https://github.com/BigCoke233/" class="no-linkTarget">Eltrac</a></li>
+      <li>Powered by <a rel="nofollow" target="_blank" href="http://www.typecho.org" class="no-linkTarget">Typecho</a></li>
+    </ul>
+  </section>
+</div>
+<?php endif; ?>
 <!-- 返回顶部按钮 -->
 <div id="gotop">
     <button id="back2top"><span class="iconfont">&#xe749;</span></button>
