904efa26f9821cceb826e10e798580b72b03fe7b
Eltrac Koalar <hi@guhub.cn>
2022-08-23 21:47:30 +0800

    :bug: Fix: 语法错误

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('Matcha',
 
 function themeInit($archive) {
     if ($archive->hidden) header('HTTP/1.1 200 OK');//暴力解决访问加密文章会被 pjax 刷新页面的问题
-    Helper::options()->commentsAntiSpam = false; 关闭反垃圾
+    Helper::options()->commentsAntiSpam = false; //关闭反垃圾
     Helper::options()->commentsMaxNestingLevels = '9999';//最大嵌套层数
     Helper::options()->commentsPageDisplay = 'first';//强制评论第一页
     Helper::options()->commentsOrder = 'DESC';//将最新的评论展示在前
