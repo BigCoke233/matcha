6f192c319fd38960cd863fc8383154e05ae20661
Eltrac Koalar <hi@guhub.cn>
2022-08-23 12:32:22 +0800

    :airplane: Update: 更新 toaster.js

--- assets/toaster/toaster.js
+++ assets/toaster/toaster.js
@@ -2,50 +2,64 @@
  * Toaster.js
  * 
  * Repository: https://github.com/BigCoke233/toaster.js
- * Version: 0.1
+ * Version: 1.0.0
  * 
  * jQuery required!
  */
 
-(function(){
-
-    Toaster = {};
+Toaster = function() {
 
     Toaster.amount = 0;
 
-    Toaster.send = function(m, color='rgb(197,197,106)') {
-        Toaster.dismiss('#toast-'+Toaster.amount);
+    Toaster.default = {
+        color: '#C5C56A',
+        autoClose: true,
+        autoCloseDelay: 2000,
+        position: 'right-top'
+    };
 
+    //core method
+    Toaster.toast = function(m, options = Toaster.default){
+        if($('#toast-'+Toaster.amount).length) Toaster.dismiss('#toast-'+Toaster.amount);
         Toaster.amount++;
 
         var id = 'toast-'+Toaster.amount;
         var selector = '#'+id;
-        $toast = $('<div class="toaster"></div>').attr('id',id).text(m).css({'background': color});
+        $toast = $('<div class="toaster"></div>').attr('id',id).text(m).css({'background': options.color || Toaster.default.color}).addClass('toaster-'+(options.position || Toaster.default.position));
         $('body').append($toast);
         setTimeout(function(){
             $(selector).addClass('toasting');
         }, 1);
 
         $(selector).click(function(){
+            if(options.onClick) options.onClick();
             Toaster.dismiss(selector);
         });
 
-        setTimeout(function(){
-            Toaster.dismiss(selector);
-        }, 2000);
-    };
+        var autoClose=Toaster.default.autoClose;
+        if(options.autoClose!==undefined) autoClose=options.autoClose;
+        if(autoClose) {
+            setTimeout(function(){
+                Toaster.dismiss(selector);
+            }, options.autoCloseDelay || Toaster.default.autoCloseDelay);
+        }
+    }
+    Toaster.send = Toaster.toast; //for downward compatibility
 
-    Toaster.error = function(m) {
-        Toaster.send(m, 'rgb(244,66,54)');
+    //quick method
+    Toaster.error = function(m, options) {
+        Toaster.default.color='#F44236';
+        Toaster.toast(m, options);
+        Toaster.default.color='#C5C56A';
     }
 
+    //dismiss given toast
     Toaster.dismiss = function(toast) {
         $(toast).removeClass('toasting').addClass('toast-dismissed');
         setTimeout(function(){
             $(toast).remove();
         }, 1000);
     }
+};
 
-    return Toaster;
-
-})();
+new Toaster();
