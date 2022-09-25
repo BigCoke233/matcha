e5e1ca64d04e5077269acb92f0f97f4c3a405a0c
Eltrac Koalar <hi@guhub.cn>
2022-09-25 14:47:27 +0800

    :lock: Add: 为密码保护文章的表单添加样式

--- functions.php
+++ functions.php
@@ -14,7 +14,6 @@ Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Matcha',
 Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('Matcha','parseContent');
 
 function themeInit($archive) {
-    if ($archive->hidden) header('HTTP/1.1 200 OK');//暴力解决访问加密文章会被 pjax 刷新页面的问题
     Helper::options()->commentsAntiSpam = false; //关闭反垃圾
     Helper::options()->commentsMaxNestingLevels = '9999';//最大嵌套层数
     Helper::options()->commentsPageDisplay = 'first';//强制评论第一页
